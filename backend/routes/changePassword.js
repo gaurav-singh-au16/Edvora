b60212b2a46b3e6eac9fbb781a2c91644f388e47
Gaurav Singh <gauravsingh.info@gmail.com>
2021-08-16 17:24:28 +0530

    created changePassword route and implemented the logic

--- backend/routes/changePassword.js
+++ backend/routes/changePassword.js
@@ -1,9 +1,41 @@
 const express = require('express')
 const router = express.Router()
+const UserModel = require('../models/users')
+const bcrypt = require('bcryptjs')
 
 
-router.get('/changePassword', (req, res) => {
-    res.json({success: 'changePassword Is Working'})
+router.post('/changePassword', (req, res) => {
+    try {
+        const { email, password, newPassword } = req.body
+
+        if (!(email && password, newPassword)) {
+        res.status(400).send("All input is required")
+        }
+
+        const user = await UserModel.findOne({ email })
+    
+        if (user && (await bcrypt.compare(password, user.password))) {
+            
+            encryptedPassword = await bcrypt.hash(newPassword, 10)
+            const changePassword = await UserModel.findByIdAndUpdate(user._id, { password: encryptedPassword })
+
+            const token = jwt.sign(
+                { user_id: user._id, email },
+                SECERET_KEY,
+                {expiresIn: "2h"})
+    
+          
+            user.token = token;
+    
+            res.status(200).json(user);
+        }else{
+
+            res.status(400).send("Invalid Credentials")
+        }
+
+    } catch (err) {
+        console.log(err);
+    }
 })
 
 
